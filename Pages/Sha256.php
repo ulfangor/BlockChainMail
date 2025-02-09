bdb1d1613823f5737ebcec662975d749ffdf3f8f
ulfangor <inesanzil@gmail.com>
2025-02-09 15:23:27 +0100

    sha (système fonctionnel // design à faire)

--- Pages/Sha256.php
+++ Pages/Sha256.php
@@ -7,6 +7,28 @@
     <title>SHA-256</title>
 </head>
 <body>
-    
+    <h1>BlockChainMail</h1>
+    <h2>SHA-256</h2>
+
+    <form method="POST" action="">
+        <div class="input-container">
+            <textarea name="rawText" placeholder="Entrez votre texte ici..."></textarea>
+            <button type="submit" name="hashButton">HASH</button>
+            <div class="output">
+                <?php
+                if (isset($_POST['hashButton'])) {
+                    // Récupérer le texte saisi
+                    $rawText = $_POST['rawText'];
+
+                    // Hasher le texte en SHA-256
+                    $hashedText = hash('sha256', $rawText);
+
+                    // Afficher le résultat
+                    echo htmlspecialchars($hashedText);
+                }
+                ?>
+            </div>
+        </div>
+    </form>
 </body>
 </html>
