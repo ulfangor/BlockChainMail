979518f5d1b44382a10571ed519f1d55b910bb49
ulfangor <inesanzil@gmail.com>
2025-02-09 14:55:58 +0100

    Création des pages

---
+++ Pages/Sha256.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Styles/sha256.css">
+    <title>SHA-256</title>
+</head>
+<body>
+    
+</body>
+</html>
