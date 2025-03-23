6f9cab66f7ee27ae99f039b8fc6e1beebba8f6c6
ulfangor <inesanzil@gmail.com>
2025-03-23 20:06:43 +0100

    rien de spécial

---
+++ Pages/Mempool.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BlockChainMail</title>
+</head>
+<body>
+    <?php
+        include '../Pages.Header.php';
+    ?>
+</body>
+</html>
